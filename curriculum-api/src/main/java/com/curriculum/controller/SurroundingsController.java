71e3abe01b85624ca9b02b2bf9f31155e443b200
zilan <369496090@qq.com>
2024-12-17 21:28:08 +0800

    周边信息查询

--- curriculum-api/src/main/java/com/curriculum/controller/SurroundingsController.java
+++ curriculum-api/src/main/java/com/curriculum/controller/SurroundingsController.java
@@ -1,8 +1,12 @@
 package com.curriculum.controller;
 
+import com.curriculum.annotation.Anonymous;
+import com.curriculum.model.vo.RestResponse;
 import com.curriculum.service.SurroundingsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
-@RequestMapping("surroundings")
+@RequestMapping("/api/curriculum/surroundings/")
 public class SurroundingsController {
 
     @Autowired
     private SurroundingsService  surroundingsService;
+
+    @Anonymous
+    @GetMapping("info/{id}")
+    public RestResponse info(@PathVariable Integer id) {
+        log.info("周边信息查询：{}", id);
+        return RestResponse.success(surroundingsService.getById(id));
+    }
 }
