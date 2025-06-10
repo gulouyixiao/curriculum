fbeabcc16e325c600098d2b3602a1b50358a84c7
zilan <369496090@qq.com>
2025-06-10 20:54:59 +0800

    添加的查看购物车内详细的物品信息

--- curriculum-api/src/main/java/com/curriculum/controller/ShoppingCartController.java
+++ curriculum-api/src/main/java/com/curriculum/controller/ShoppingCartController.java
@@ -1,9 +1,14 @@
 package com.curriculum.controller;
 
 
+import com.curriculum.annotation.Anonymous;
+import com.curriculum.model.po.Acgn;
 import com.curriculum.model.po.ShoppingCart;
+import com.curriculum.model.po.Surroundings;
 import com.curriculum.model.vo.RestResponse;
+import com.curriculum.service.AcgnService;
 import com.curriculum.service.ShoppingCartService;
+import com.curriculum.service.SurroundingsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -11,15 +16,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
+@Anonymous
 @Slf4j
 @RestController
-@RequestMapping("/api/curriculum/cart")
+@RequestMapping("/api/curriculum/cart/")
 @Api(tags = "购物车表")
 public class ShoppingCartController {
 
     @Autowired
     private ShoppingCartService shoppingCartService;
+
+    @Autowired
+    private SurroundingsService surroundingsService;
+
+    @Autowired
+    private AcgnService gnService;
 
 
     @GetMapping("remove/{id}")
@@ -48,4 +61,19 @@ public class ShoppingCartController {
     }
 
     //查看购物车商品详情
+    @ApiOperation("查看商品详细")
+    @GetMapping("/{id}")
+    public RestResponse<Object> getById(@PathVariable Integer id) {
+        ShoppingCart shoppingCart = shoppingCartService.getById(id);
+        Long shopId = shoppingCart.getShoppingId();
+        if (Objects.equals(shoppingCart.getShoppingType(), "01")) {
+            Surroundings surroundings = surroundingsService.getById(shopId);
+            return RestResponse.success(surroundings);
+        }else if (Objects.equals(shoppingCart.getShoppingType(), "02")) {
+            Acgn gn = gnService.getById(shopId);
+            return RestResponse.success(gn);
+        }
+        return RestResponse.validfail("不合法的类型");
+//        return RestResponse.success(shoppingCart);
+    }
 }
